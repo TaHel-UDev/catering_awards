8d9b0ae0d8b2db4b88e127db19a73c4e50e21a62
TaHel <rusdigits@yandex.ru>
2026-03-26 20:51:05 +0500

    Update

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ru">
-      <Head />
+      <Head>
+        {/* AutoSchema ONE-TIME STATIC MODE (CDN-only) */}
+        <script
+          src="https://www.autoschema.app/cdn/s.js"
+          data-key="sk_59586a3406dc6b098df390f651b8cbdd"
+          data-api="https://www.autoschema.app"
+          data-static-base="https://cdn.autoschema.app/schemas"
+          data-static-only="1"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
